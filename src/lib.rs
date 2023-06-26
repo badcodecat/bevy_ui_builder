da1c333e3ad7c24970b857e3e01e8eda50e08532
KittenDeveloper <28487280+KittenDeveloper@users.noreply.github.com>
2023-06-26 00:02:05 -0500

    get things ready

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod prelude;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+mod tests
+{
+	use super::*;
 }
