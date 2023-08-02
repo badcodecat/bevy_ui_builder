9d1ac5d26c20b2c21e2c18f9eb301bf564dd39b9
badcodecat <28487280+badcodecat@users.noreply.github.com>
2023-08-02 17:37:22 -0500

    change the size of LARGE

--- src/theme/dimensions.rs
+++ src/theme/dimensions.rs
@@ -2,4 +2,4 @@
 
 	pub const NONE: UiRect = UiRect::new(Val::Px(0f32), Val::Px(0f32), Val::Px(0f32), Val::Px(0f32));
 	pub const SMALL: UiRect = UiRect::new(Val::Percent(0.5f32), Val::Percent(0.5f32), Val::Percent(0.5f32), Val::Percent(0.5f32));
-	pub const LARGE: UiRect = UiRect::new(Val::Percent(10f32), Val::Percent(10f32), Val::Percent(10f32), Val::Percent(10f32));
+	pub const LARGE: UiRect = UiRect::new(Val::Percent(5f32), Val::Percent(5f32), Val::Percent(5f32), Val::Percent(5f32));
