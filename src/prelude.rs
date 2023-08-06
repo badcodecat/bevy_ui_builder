9a0ff5fef32f3182002f42e6c2705f317174ac7a
badcodecat <28487280+badcodecat@users.noreply.github.com>
2023-08-05 19:08:15 -0500

    update prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,6 +4,7 @@ pub use crate::widgets::ParentData;
 
 // Needed to query for widget contents:
 pub use crate::widgets::text_input::EditableText;
+pub use crate::widgets::checkbox::CheckBoxState;
 
 pub use crate::theme::CurrentThemeData;
 pub use crate::theme::Theme;
